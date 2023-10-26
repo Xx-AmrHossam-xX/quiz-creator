3fc1ea73acbf952806c8100d8c61be97e64fe85c
XxAmrHossamxX <amrhossam321@gmail.com>
2023-10-26 21:13:08 +0200

    Update UI of quiz list

--- src/components/QuizesList/QuizesList.jsx
+++ src/components/QuizesList/QuizesList.jsx
@@ -4,15 +4,26 @@ const QuizesList = ({ quizes, toggleForm }) => {
   return (
     <div className="list-group m-3">
       {quizes.map((quiz) => {
+        const { title, description, score, url } = quiz
         return (
-          <button
-            type="button"
-            key={quiz.id}
-            className="list-group-item list-group-item-action quiz-item"
-            onClick={() => toggleForm(quiz)}
-          >
-            {quiz.title}
-          </button>
+          <div class="quiz-card card mb-3">
+            <div class="card-body">
+              <h5 class="card-title"> {title}</h5>
+              <p class="card-text">{`score : ${score}`}</p>
+              <p class="card-text">{description}</p>
+              <a href={url} target="_blank" class="card-link" rel="noreferrer">
+                Url
+              </a>
+              <button
+                type="button"
+                key={quiz.id}
+                className="list-group-item list-group-item-action quiz-item"
+                onClick={() => toggleForm(quiz)}
+              >
+                Update
+              </button>
+            </div>
+          </div>
         )
       })}
     </div>
